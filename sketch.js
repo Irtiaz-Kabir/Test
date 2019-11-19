708a5198a7c6c6c4b0716936c5ce6d404ad36ed9
Md Irtiaz Kabir <codingislife.code@gmail.com>
2019-11-19 12:52:44 +0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,5 @@ var dropdown;
 
 function setup() {
 	noCanvas();
-	dropdown = createSelect().size(300,50).style('font-size','30pt');
-	dropdown.option('Alkane');
-	dropdown.option('Alkene');
-	dropdown.option('Alkyne');
-	dropdown.changed(para);
-}
-
-function para() {
-	let str = dropdown.value();
-	createP("It is " + str);
+	createInput();
 }
