99d3022d293fc988e84a17de1c79245f51673a13
Md Irtiaz Kabir <codingislife.code@gmail.com>
2019-11-21 21:06:56 +0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ var output;
 function setup() {
   createCanvas(400,400);
   background(100,200,255);
-  // inputBox = createInput();
-  // inputBox.position(100,100);
-  // inputBox.input(para);
-  // output = createP('');
+  inputBox = createInput();
+  inputBox.position(100,100);
+  inputBox.input(para);
+  output = createP('');
 
   createElement('h1','Is it working?');
 }
